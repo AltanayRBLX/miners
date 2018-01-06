c1bc7e6e4d4aed0890008c5f4a53b1210c9720ad
AltanayRBLX <35113904+AltanayRBLX@users.noreply.github.com>
2018-01-06 21:11:18 +0300

    Add files via upload

---
+++ miner.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = 'm!' ;
+
+client.on('ready', () => {
+  console.log(`BOT: ${client.user.tag} adıyla giriş yapıldı!`);
+  client.user.setGame('Miners V1 m!ping');
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'ping') {
+    msg.reply('**Pong!** '  +  client.ping);
+  }
+  if (msg.content === 'hi') {
+    msg.reply('**Welcome**');
+    msg.react('🍵');
+    }
+  }
+});
+
+client.login('Mzk2MzA1NjczMTkwODk5NzE0.DSff8g.9VKjrYEgXtkw-BQ5olTjB8VG7YU');
+   
